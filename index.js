5d478de9104f4ae89061be0d502a160c3e693073
Cat <cchiong1597@gmail.com>
2022-09-17 15:02:23 -0400

    Add hide slides function

--- index.js
+++ index.js
@@ -20,11 +20,6 @@
     6) Use console.log to validate that you have 3 'carousel-item' in your 'slides' const
 */
 
-//Adding event listeners to grab all carousel items
-
-
-
-
 /*const slides = document.getElementsByClassName('carousel-item'); //Adding event listener to retrieve photos by class name
 
 let slidePosition = 0; ////The starting point of the slide index at 0 //want it to change
@@ -34,26 +29,6 @@ const totalSlides = slides.length; // length of the slides
 document.getElementById('carousel-button-prev').addEventListener("click", moveToNextSlide); //button for event listener
 
 document.getElementById('carousel-button-next').addEventListener("click", moveToPrevSlide); //button for event listener
-
-function moveToNextSlide() {
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    
-    slides[slidePosition].classList.add("carousel-item-visible");
-}
-
-function moveToPrevSlide() {
-    
-}
-
-
-
-
-
-
 
 // function moveToNextSlide() {
   
@@ -79,30 +54,6 @@ function moveToPrevSlide() {
 //console.log(totalSlides)
 
 
-/*
-    Moving to the next slide!
-    
-    1) Check if (slidePosition === totalSlides -1) (yes minus 1 because we start from 0 on 'slidePosition' - 
-    so if the slide is the last one (after a user has clicked through all the 
-        slides and then hits next again) we must reset the 'slidePosition' to 0, 
-        in turn taking them back to the first slide! 
-    1: Info) Why reset? well, if we try access 'slides[slidePosition]' as such 'slides[3]' or 'slides[4]' or 'slides[5]', 
-    these don't exist (we have 3 carousel items), so we will get an error! We have [slide0, slide1, slide2] <- remember our const 'slides'? 
-    That is has an array structure in that the items index start from 0! In a none programming world, we start from 1 when counting, 
-    but this is programming, so we start counting from 0 😄
-
-    2) Once we've done our check for slidePosition against totalSlides -1 (if this conditional is true) we reset 'slidePosition' to 0, 
-    if this conditional is not true, we increment the 'slidePosition' by 1, so we go to the next slide
-
-    3) Within our function 'moveToNextSlide' we have a conditional, after the conditional, we want to add the class name of 'carousel-item-visible' 
-    to our current slide! 'carousel-item-visible' 
-    is our CSS class that makes the 'carousel-item' a block element (and not hidden) and shows it!
-
-    3.1) Use 'slidePosition' to access the current array element in 'slides', and then add the class name 'carousel-item-visible', 
-    accessing an array element is as simple as 'slides[slidePosition]'
-    4) Test out your next button by clicking on it, you'll see there's some clean up to do in the next Scrimba cast!
-*/
-
 const slides = document.getElementsByClassName('carousel-item');
 let slidePosition = 0;
 const totalSlides = slides.length;
@@ -111,6 +62,9 @@ document.getElementById('carousel-button-next').addEventListener('click', moveTo
 document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
 
 function moveToNextSlide() {
+
+    hideAllSlides();
+
     if (slidePosition === totalSlides - 1) {
         slidePosition = 0;
     } else {
@@ -121,5 +75,12 @@ function moveToNextSlide() {
 }
 
 function moveToPrevSlide() {
-    
+    slides[slidePosition].classList.remove("carousel-item-visible")
+}
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add('carousel-item-hidden');
+    }
 }
